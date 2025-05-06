e4f42c06572c9f1bae4cc37dc9beb1aee33927f3
Kool Arpan <koolrapperarpan@gmail.com>
2025-05-06 14:13:19 +0545

    basics

---
+++ basics/input-output.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter a number: ";
+    cin >> a >> b;
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b;
+    return 0;
+}
