1bd22a1ca5c511ef16cd5de0feeb1b3591232d0e
Arpan Kumar Pariyar <officialkoolarpan@gmail.com>
2025-05-15 13:48:48 +0545

    add: swap numbers

---
+++ home-assignment/swap-numbers-pointer.cpp
@@ -0,0 +1,20 @@
+// WAP to swap two numbers using pointer
+
+#include <iostream>
+void swapNumbers(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a, b;
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    swapNumbers(&a, &b);
+    std::cout << "\nSwapping two values: " << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    return 0;
+}
