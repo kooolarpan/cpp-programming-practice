4e2db978474f5e08c0056cee0005614f8c805a31
Arpan Kumar Pariyar <officialkoolarpan@gmail.com>
2025-06-02 14:38:40 +0545

    add: constructor inheritance

---
+++ home-assignment/constructor-inheritance.cpp
@@ -0,0 +1,10 @@
+/*
+Use constructors to initalize the data members.
+Define members function named ShowData() in each class to display the
+information. Create two objects of Hero class and dislay the info
+
+
+ Batman (power)  - Superman (strength)
+ Hero (life)
+
+*/
