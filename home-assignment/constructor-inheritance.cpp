941a94d2ae72ed588204907ecabbd38041b69c84
Arpan Kumar Pariyar <officialkoolarpan@gmail.com>
2025-06-02 16:20:36 +0545

    edit: assignment done

--- home-assignment/constructor-inheritance.cpp
+++ home-assignment/constructor-inheritance.cpp
@@ -8,3 +8,40 @@ information. Create two objects of Hero class and dislay the info
  Hero (life)
 
 */
+
+#include <iostream>
+using namespace std;
+
+class Batman {
+    int power;
+
+   public:
+    Batman(int p) { power = p; }
+    void display() { cout << "Batman's Power: " << power << endl; }
+};
+
+class Superman {
+    int strength;
+
+   public:
+    Superman(int s) { strength = s; }
+    void display() { cout << "Superman's Strength: " << strength << endl; }
+};
+
+class Hero : public Batman, public Superman {
+    int life;
+    public:
+    Hero(int p, int s, int l):Batman(p),Superman(s){
+        life = l;
+    }
+    void display() { cout << "Hero's Life: " << life << endl; }
+
+};
+
+int main() {
+    Hero h(5,6,10);
+    h.Batman::display();
+    h.Superman::display();
+    h.display();
+    return 0;
+}
